b5d49f872c394e27a47052ddebf3d6903de25095
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-23 08:22:01 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/javae/xecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,13 @@
+package javae.xecutavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é u método auto executavl em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java do Curso Formação Java Web");
+		System.out.println("Java é Orientado a Objetos");
+	}
+
+}
