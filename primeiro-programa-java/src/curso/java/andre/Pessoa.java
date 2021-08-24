ff1fde09f0a1c161b37d6a78b676278d463ed7c8
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-23 21:48:40 -0300

    Inclusão da Idade

--- primeiro-programa-java/src/curso/java/andre/Pessoa.java
+++ primeiro-programa-java/src/curso/java/andre/Pessoa.java
@@ -6,6 +6,16 @@ public class Pessoa {
 	
 	private String sobrenome;
 	
+	private int idade;
+	
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
