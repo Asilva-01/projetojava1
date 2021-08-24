0607d939b13d61fc107c967e7b0305a7863ffe9b
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-23 21:44:02 -0300

    Alteração e criação da classe pessoa

---
+++ primeiro-programa-java/src/curso/java/andre/Pessoa.java
@@ -0,0 +1,18 @@
+package curso.java.andre;
+
+public class Pessoa {
+
+	private String nome;
+	
+	private String sobrenome;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+}
+
