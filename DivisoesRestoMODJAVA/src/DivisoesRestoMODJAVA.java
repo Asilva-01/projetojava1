6f9bb0043fcf980da7a75b086ae19061bdafc302
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-25 23:17:16 -0300

    Divisões e resto com MOD do Java

---
+++ DivisoesRestoMODJAVA/src/DivisoesRestoMODJAVA.java
@@ -0,0 +1,14 @@
+
+public class DivisoesRestoMODJAVA {
+	/*Módulo: Resto da divisão %*/
+	
+	public static void main(String[]args) {
+		
+		double carro = 15;
+		double pessoa = 6;
+		
+		double resto = carro % pessoa; /*MOD*/
+		
+		System.out.println("Sobrema exaamente: " + resto + " Carros.");
+	}
+}
