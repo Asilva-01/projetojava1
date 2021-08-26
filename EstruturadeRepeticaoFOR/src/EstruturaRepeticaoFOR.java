bea234a19d27fb148cdc034555c72db19d36e477
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-25 22:01:10 -0300

    Estrutura de repetição FOR

---
+++ EstruturadeRepeticaoFOR/src/EstruturaRepeticaoFOR.java
@@ -0,0 +1,17 @@
+
+public class EstruturaRepeticaoFOR {
+
+	public static void main(String[]args) {
+		
+		/*Estrutura de repetições FOR*/
+		
+	
+		
+	for (int numero = 0; numero <= 10; numero ++) {
+		System.out.println("Crescente - Número atual é : " + numero);
+	
+	
+	}
+	
+	}
+}
