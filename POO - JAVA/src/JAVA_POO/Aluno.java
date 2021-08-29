7d0353678aac33511dddd41d8106e8d12db94a0f
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-08-29 19:47:22 -0300

    Os metódos comuns Getters e Setters

--- POO - JAVA/src/JAVA_POO/Aluno.java
+++ POO - JAVA/src/JAVA_POO/Aluno.java
@@ -39,5 +39,78 @@ public class Aluno {
 	public String getNome() {
 		return nome;
 	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
 	
 }
--- POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
+++ POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
@@ -12,15 +12,43 @@ public class PrimeiraClasse {
 		/*aluno1 é uma referencia para o objeto aluno*/
 		
 		Aluno aluno1 = new Aluno();/*Aqui será o João*/
-		
-		aluno1.nome = "João";
-		aluno1.idade = 50;
 
-		System.out.println("Nome no aluno 1 é = " + aluno1.nome);
-		System.out.println("A idade do aluno 1 é = " + aluno1.idade);
+		aluno1.setNome("João da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("18/10/1987");
+		aluno1.setRegistroGeral("44554.45544.545");
+		aluno1.setNumeroCpf("215.337.655-55");
+		aluno1.setNomeMae("Ana Rosa");
+		aluno1.setNomePai("Raimundo");
+		aluno1.setDataMatricula("10/01/2021");
+		aluno1.setSerieMatriculado("5");
+		aluno1.setNomeEscola("JDEV Treinamento");
 		
+		System.out.println("Nome é = " + aluno1.getNome());
+		System.out.println("Idade é = " + aluno1.getIdade());
+		System.out.println("A data de Nascimento é = " + aluno1.getDataNascimento());
+		System.out.println("A escola é = " + aluno1.getNomeEscola());
+		/*=====================================================*/
+		
+		System.out.println("------------------------------------------------------");
 		
 		Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
+		aluno2.setNome("Pedro");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("18/10/1981");
+		aluno2.setRegistroGeral("44554.45544.545");
+		aluno2.setNumeroCpf("215.447.555-55");
+		aluno2.setNomeMae("Ana Lucia");
+		aluno2.setNomePai("Juares");
+		aluno2.setDataMatricula("10/02/2021");
+		aluno2.setSerieMatriculado("6");
+		aluno2.setNomeEscola("JDEV Treinamento");
+		
+		System.out.println("Nome é do aluno 2 = " + aluno2.getNome());
+		System.out.println("Idade do " + aluno2.getNome() + " é : "+ aluno2.getIdade());
+		System.out.println("A data de Nascimento do aluno 2 é = " + aluno2.getDataNascimento());
+		System.out.println("A escola do aluno2 é  = " + aluno2.getNomeEscola());
+		
 		
 		Aluno aluno3 = new Aluno();/*Aqui será o Andre*/
 		
