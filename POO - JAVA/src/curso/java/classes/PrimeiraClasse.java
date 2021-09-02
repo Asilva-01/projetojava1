b207cb7b12b7fbdea9efd3af826173699f6d3dd4
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-02 20:47:24 -0300

    Remover disciplinas em repetição

--- POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
+++ POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
@@ -53,9 +53,19 @@ public class PrimeiraClasse {
 
 		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina ?");
 		
-		if (escolha == 0) {
-			String disciplinaRemover = JOptionPane.showInputDialog("Qual a disciplina 1, 2, 3, ou 4  ?");
-			aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue()-1);
+		if (escolha == 0) {/*Opção SIM é ZERO*/
+			
+			int continuarRemover = 0; 
+			int posicao = 1;
+			while (continuarRemover == 0) {
+				
+			String disciplinaRemover = JOptionPane.showInputDialog
+					("Qual a disciplina 1, 2, 3, ou 4 ?");
+			aluno1.getDisciplinas().remove(Integer.valueOf
+			(disciplinaRemover).intValue()- posicao);
+			posicao ++;
+			continuarRemover = JOptionPane.showConfirmDialog
+			(null, "Continuar a remover? ");
 		}
 		
 		
@@ -94,4 +104,5 @@ public class PrimeiraClasse {
 		Aluno aluno5 = new Aluno("José", 50);
 	}
 
+	}
 }
