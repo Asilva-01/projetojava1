119c8a0bd30fc3595019dc682dfd563991807fb7
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-02 21:42:31 -0300

    Criando e Percorrendo a lista de alunos

--- POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
+++ POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
@@ -1,5 +1,8 @@
 package curso.java.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import JAVA_POO.Aluno;
@@ -9,13 +12,16 @@ public class PrimeiraClasse {
 	
 	public static void main(String[]args) {
 		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		for (int qtd = 1 ; qtd <=2; qtd++) {
 		
 		
 		/*new Aluno() é uma instancia (Criaçao de Objeto)*/
 		/*aluno1 é uma referencia para o objeto aluno*/
 		
-		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
-		String idade = JOptionPane.showInputDialog("Qual a idade?");
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno "+qtd+" ?");
+		/*String idade = JOptionPane.showInputDialog("Qual a idade?");
 		String dataNascimento = JOptionPane.showInputDialog("Data de nascimento?");
 		String rg = JOptionPane.showInputDialog("Registro Geral?");
 		String cpf = JOptionPane.showInputDialog("Informe o número do CPF");
@@ -23,13 +29,13 @@ public class PrimeiraClasse {
 		String pai = JOptionPane.showInputDialog("Nome do Pai");
 		String matricula = JOptionPane.showInputDialog("Data de matrícula");
 		String serie = JOptionPane.showInputDialog("Qual a série do aluno?");
-		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");
+		String escola = JOptionPane.showInputDialog("Qual o nome da escola?");*/
 		
 		
 		Aluno aluno1 = new Aluno();/*Aqui será o João*/
 
 		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade));
+		/*aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setRegistroGeral(rg);
 		aluno1.setNumeroCpf(cpf);
@@ -37,7 +43,7 @@ public class PrimeiraClasse {
 		aluno1.setNomePai(pai);
 		aluno1.setDataMatricula(matricula);
 		aluno1.setSerieMatriculado(serie);
-		aluno1.setNomeEscola(escola);
+		aluno1.setNomeEscola(escola);*/
 		
 		for (int pos = 1; pos <=4; pos++) {
 			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+pos+" ?");
@@ -67,19 +73,30 @@ public class PrimeiraClasse {
 			continuarRemover = JOptionPane.showConfirmDialog
 			(null, "Continuar a remover? ");
 		}
+			
+			
+		}
+			alunos.add(aluno1);
+		}
+			for (Aluno aluno : alunos ) {
+				
+				System.out.println(aluno);
+				System.out.println(aluno.getDisciplinas());
+				System.out.println("A média é = " + aluno.getMediaNota());
+				System.out.println("Resultado = " + aluno.getAlunoAprovado2());
+				System.out.println("-----------------------------------------");
+			}
 		
-		
-		System.out.println(aluno1);
-		System.out.println(aluno1.getDisciplinas());
-		System.out.println("A média é = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
-		
+		}
+	}
+
+			
 		/*=====================================================*/
 		
-		System.out.println("------------------------------------------------------");
+		/*System.out.println("------------------------------------------------------");*/
 		
-		Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
-		aluno2.setNome("Pedro");
+		/*Aluno aluno2 = new Aluno();/*Aqui será o Pedro*/
+		/*aluno2.setNome("Pedro");
 		aluno2.setIdade(40);
 		aluno2.setDataNascimento("18/10/1981");
 		aluno2.setRegistroGeral("44554.45544.545");
@@ -99,10 +116,10 @@ public class PrimeiraClasse {
 		
 		Aluno aluno3 = new Aluno();/*Aqui será o Andre*/
 		
-		Aluno aluno4 = new Aluno("Maria");
+		/*Aluno aluno4 = new Aluno("Maria");
 		
-		Aluno aluno5 = new Aluno("José", 50);
-	}
+		Aluno aluno5 = new Aluno("José", 50);*/
+	
 
-	}
-}
+	
+
