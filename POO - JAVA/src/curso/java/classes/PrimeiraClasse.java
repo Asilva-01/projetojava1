68024e94272b1ebcf49287bcae9d78a0e7084106
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-04 18:17:36 -0300

    Substituindo um aluno na lista

--- POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
+++ POO - JAVA/src/curso/java/classes/PrimeiraClasse.java
@@ -45,7 +45,7 @@ public class PrimeiraClasse {
 		aluno1.setSerieMatriculado(serie);
 		aluno1.setNomeEscola(escola);*/
 		
-		for (int pos = 1; pos <=4; pos++) {
+		for (int pos = 1; pos <=1; pos++) {
 			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina "+pos+" ?");
 			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina "+pos+" ?");
 			
@@ -80,6 +80,20 @@ public class PrimeiraClasse {
 		}
 			for (int pos = 0; pos < alunos.size(); pos ++) {
 				Aluno aluno = alunos.get(pos);
+				
+				if (aluno.getNome().equalsIgnoreCase("Andre")) {
+					Aluno trocar = new Aluno();
+					trocar.setNome("Aluno foi trocado");
+					
+					Disciplina disciplina = new Disciplina();
+					disciplina.setDisciplina("Matematica");
+					disciplina.setNota(96);
+					
+					trocar.getDisciplinas().add(disciplina);
+					
+					alunos.set(pos, trocar);
+					aluno = alunos.get(pos);
+				}
 				
 				System.out.println("Aluno = " + aluno.getNome());
 				System.out.println("Media do aluno = " + aluno.getMediaNota());
