0d63f2ae8db943ce9178d0d2ec0de314231c9912
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-14 20:01:16 -0300

    Criando uma Matriz multidimensional

---
+++ POO - JAVA/src/curso/java/classes/Matriz.java
@@ -0,0 +1,23 @@
+package curso.java.classes;
+
+public class Matriz {
+	
+	/*Executar o codigo Java*/
+	public static void main(String[] args) {
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80;
+		notas[0][1] = 90;
+		notas[0][2] = 70;
+		
+		notas[1][0] = 40;
+		notas[1][1] = 60;
+		notas[1][2] = 30;
+		
+		
+		
+		System.out.println(notas);
+	}
+
+}
