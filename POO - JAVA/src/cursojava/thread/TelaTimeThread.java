cb8e916ddebed91f8999044b30886a3ee417d779
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-15 22:30:14 -0300

    Conhecendo o GridBagConstraints gerenciador de posicionamento e layout

--- POO - JAVA/src/cursojava/thread/TelaTimeThread.java
+++ POO - JAVA/src/cursojava/thread/TelaTimeThread.java
@@ -1,10 +1,25 @@
 package cursojava.thread;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class TelaTimeThread  extends JDialog{
+	
+	private JPanel jPanel = new JPanel(new GridBagLayout()); /*Painel de componentes*/
+	
+	private JLabel descricaoHora = new JLabel("Time Thread 1");
+	private JTextField mostraTempo = new JTextField();
+	
+	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
+	private JTextField mostraTempo2 = new JTextField();
+	
 	
 	public TelaTimeThread() { /*Executa o que tiver dentro no momento da abertura ou execução*/
 		setTitle("Minha tela de time com Thread");
@@ -13,7 +28,27 @@ public class TelaTimeThread  extends JDialog{
 		setResizable(false);
 		/*Primeira parte concluida*/
 		
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();/*Controlar posicionamento*/
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
 		
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+		
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy ++;	
+		jPanel.add(mostraTempo, gridBagConstraints);
+		
+		
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+		
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;	
+		jPanel.add(mostraTempo2, gridBagConstraints);		
+		
+		add(jPanel, BorderLayout.WEST);
 		/*Smepre será o último comando*/
 		setVisible(true);/*Torna a tela visível para o usuário*/
 		
