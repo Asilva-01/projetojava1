eef832c5147eab4a53628abfc6b1a914771becf2
Andre Luiz da Silva <andre1981luiz@gmail.com>
2021-09-16 20:38:37 -0300

    Controlando a Thead 2 com Start e Stop

--- POO - JAVA/src/cursojava/thread/TelaTimeThread.java
+++ POO - JAVA/src/cursojava/thread/TelaTimeThread.java
@@ -50,8 +50,27 @@ public class TelaTimeThread  extends JDialog{
 		}
 	};
 	
-	private Thread thread1Time;
+private Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {/*Fica sempre rodando*/
+			while(true) {
+				mostraTempo2.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").
+						format(Calendar.getInstance().getTime()));
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+	};
 	
+	private Thread thread1Time;
+	private Thread thread2Time;
 	
 	public TelaTimeThread() { /*Executa o que tiver dentro no momento da abertura ou execução*/
 		setTitle("Minha tela de time com Thread");
@@ -106,6 +125,12 @@ public class TelaTimeThread  extends JDialog{
 				thread1Time = new Thread(thread1);
 				thread1Time.start();
 				
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+				
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+				
 			}
 		});
 		
@@ -115,10 +140,14 @@ public class TelaTimeThread  extends JDialog{
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				thread1Time.stop();
+				thread2Time.stop();
 				
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
 			}
 		});
 		
+		jButton2.setEnabled(false);
 		
 		add(jPanel, BorderLayout.WEST);
 		/*Smepre será o último comando*/
